a8b0865f7701d058bda04db28811fed214e30574
BangTtagGum <lionalmessi@naver.com>
2023-03-31 14:50:21 +0900

    Feat: LectureContent Entity 추가

---
+++ src/main/java/com/example/DoroServer/domain/lectureContent/entity/LectureContent.java
@@ -0,0 +1,34 @@
+package com.example.DoroServer.domain.lectureContent.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LectureContent {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "lecture_content_id")
+    private Long id; // PK
+
+    private String title; // 강의 제목
+
+    private String kit; // 강의 사용 키트
+
+    private String detail; // 강의 세부 구성
+
+    private String remark; // 강의 기타 사항
+
+    private String requirement; // 강의 자격 요건
+}
