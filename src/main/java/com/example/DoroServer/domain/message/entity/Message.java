1de9450d6655e5ba44957520369a9f93a33105ee
BangTtagGum <lionalmessi@naver.com>
2023-03-31 16:55:37 +0900

    Feat: Message-User 연관관계 추가
    Message와 User는 다대일(Many-to-One) 단방향  관계

--- src/main/java/com/example/DoroServer/domain/message/entity/Message.java
+++ src/main/java/com/example/DoroServer/domain/message/entity/Message.java
@@ -1,14 +1,13 @@
 package com.example.DoroServer.domain.message.entity;
 
+import com.example.DoroServer.domain.chat.entity.Chat;
+import com.example.DoroServer.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -22,10 +21,19 @@ public class Message {
     @Column(name = "message_id")
     private Long id; //PK
 
-//    private Long chatId; // 채팅방 FK
-
-//    private Long sender; // 작성한 사용자
-
     private String content; // 메시지 내용
+
+    //== 연관관계 매핑 ==//
+
+    // Message와 Chat은 다대일(Many-to-One) 관계
+//    @ManyToOne
+//    @JoinColumn(name = "chat_id")
+//    private Chat chat;
+
+    // Message와 User는 다대일(Many-to-One) 관계
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
 
 }
