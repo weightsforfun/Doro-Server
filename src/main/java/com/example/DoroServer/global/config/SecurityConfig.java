020d4975ce42acd84821516857e3df18b4b9379c
Ju-yeop <dhwnduq12@naver.com>
2023-03-31 23:51:11 +0900

    Feat: Security Conig 추가 Custom Filter 적용 전

---
+++ src/main/java/com/example/DoroServer/global/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.DoroServer.global.config;
+
+import com.example.DoroServer.global.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig{
+
+    private final JwtTokenProvider jwtTokenProvider;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // token을 사용하는 방식이기 때문에 csrf disable
+                .csrf().disable()
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/v3/api-docs").permitAll()
+                .antMatchers("/image/**").permitAll()
+                .antMatchers("/users/refresh").permitAll()
+
+                .antMatchers("/test").authenticated()
+                .antMatchers("/login/**").permitAll()
+
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .anyRequest().authenticated();
+
+                //.and()
+                //.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
