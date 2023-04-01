e8278e106db3bd05198d6813715eb567a9ee062d
Ju-yeop <dhwnduq12@naver.com>
2023-04-01 19:21:15 +0900

    Feat: User Entity 컬럼명 변경으로 인한 Repository 메서드명 변경

--- src/main/java/com/example/DoroServer/global/jwt/CustomUserDetailsService.java
+++ src/main/java/com/example/DoroServer/global/jwt/CustomUserDetailsService.java
@@ -15,7 +15,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username)
+        return userRepository.findByAccount(username)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
     }
 }
