4c0d64982a531bf3f8102ad44c45cfebd7cc290b
BangTtagGum <lionalmessi@naver.com>
2023-04-05 15:20:07 +0900

    Feat: LectureDate 값타입 컬렉션 추가
    lectureDates @ElementCollection 추가

--- src/main/java/com/example/DoroServer/domain/lecture/entity/LectureDate.java
+++ src/main/java/com/example/DoroServer/domain/lecture/entity/LectureDate.java
@@ -1,16 +1,29 @@
 package com.example.DoroServer.domain.lecture.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 @Embeddable
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LectureDate {
 
+    @ElementCollection
+    @CollectionTable(name = "lecture_date", joinColumns =
+    @JoinColumn(name = "lecture_id"))
     private List<LocalDateTime> lectureDates = new ArrayList<>(); // 강의 날짜
 //
-    private LocalDateTime enrollStateDates; // 강의 등록 시작 날짜
+    private LocalDateTime enrollStateDate; // 강의 등록 시작 날짜
     //
-    private LocalDateTime enrollEndDates; // 강의 등록 종료 날짜
+    private LocalDateTime enrollEndDate; // 강의 등록 종료 날짜
 }
