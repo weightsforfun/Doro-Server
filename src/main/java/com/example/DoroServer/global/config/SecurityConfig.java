8e0463f9f861bb22ca72c673fd252f7b6ba3261d
Ju-yeop <dhwnduq12@naver.com>
2023-04-05 18:06:14 +0900

    Feat: 필터체인에 ExceptionHandler 추가 JwtAuthenticationEntryPoint, jwtAccessDeniedHandler, JwtAuthenticationExceptionHandler 추가

--- src/main/java/com/example/DoroServer/global/config/SecurityConfig.java
+++ src/main/java/com/example/DoroServer/global/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.example.DoroServer.global.config;
 
-import com.example.DoroServer.global.jwt.JwtAuthenticationFilter;
-import com.example.DoroServer.global.jwt.JwtTokenProvider;
+import com.example.DoroServer.global.jwt.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +18,8 @@ public class SecurityConfig{
 
     private final JwtTokenProvider jwtTokenProvider;
     private final CorsConfig corsConfig;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
 
     // 필터를 거치지 않는 요청
@@ -42,12 +43,19 @@ public class SecurityConfig{
         http
                 // token을 사용하는 방식이기 때문에 csrf disable
                 .csrf().disable()
-                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+
+                /**401, 403 Exception 핸들링 */
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                /** 세션을 사용하지 않기 때문에 STATELESS로 설정 */
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
 
-                .antMatchers("/test").authenticated()
+                .antMatchers("/test").hasRole("USER")
 
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/**").hasRole("USER")
@@ -55,7 +63,8 @@ public class SecurityConfig{
 
                 .and()
                 .addFilter(corsConfig.corsFilter())
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtAuthenticationExceptionHandler(), JwtAuthenticationFilter.class);
         return http.build();
     }
 }
