ae9c2e93f5bbccdfa2d2a9ee4dec229d12810986
Ju-yeop <dhwnduq12@naver.com>
2023-04-09 01:59:29 +0900

    Fix: Redis 연결 오류 수정 RedisConnectionFactory 생성 시 password 추가 password 입력 시 RedisPassword.of() 메서드 사용

--- src/main/java/com/example/DoroServer/global/config/RedisConfig.java
+++ src/main/java/com/example/DoroServer/global/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -18,17 +20,24 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
+    @Value("${spring.redis.password}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        configuration.setPassword(RedisPassword.of(password));
+        return new LettuceConnectionFactory(configuration);
     }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer()); // Redis의 Key를 문자열로 사용
         redisTemplate.setValueSerializer(new StringRedisSerializer()); // Redis의 Value를 문자열로 사용
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
 }
