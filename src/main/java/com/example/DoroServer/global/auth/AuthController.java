d50a6f643147454dd5dbbc09262d0eed206df51f
Ju-yeop <dhwnduq12@naver.com>
2023-04-09 19:41:34 +0900

    Feat: AuthController - 아이디 중복체크 메서드 추가

--- src/main/java/com/example/DoroServer/global/auth/AuthController.java
+++ src/main/java/com/example/DoroServer/global/auth/AuthController.java
@@ -12,8 +12,10 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = "인증 🔐")
@@ -23,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final UserRepository userRepository;
     private final MessageService messageService;
+    private final AuthService authService;
 
     /**
      * ReponseEntity 사용 - header에 Token 추가
@@ -31,27 +34,35 @@ public class AuthController {
     @Operation(summary = "001_01", description = "회원가입")
     public ResponseEntity<?> join (@RequestBody @Valid JoinReq joinReq){
         /**
-         * 인증 번호 저장 -> UberEats에서 했던 것처럼 ValidationPhomeNumber 테이블을 따로 만들어야한다.
+         * 인증 번호 저장
          * 전화번호 인증을 클릭하면 레디스에 인증번호을 저장하고
-         * 인증번호 확인을 클릭하면 데디스에 저장한 인증번호와 비교해서
+         * 인증번호 확인을 클릭하면 레디스에 저장한 인증번호와 비교해서
          * 맞으면 그 인증된 전화번호를 테이블에 저장하고
          * 회원가입을 할 때 RequestBody 값으로 들어온 phoneNumber가 인증된 전화번호 테이블에 존재하는
          * 전화번호인지 확인한다.
          */
-        return ResponseEntity.ok("성공");
+
+        return ResponseEntity.ok("회원가입 완료");
     }
 
     @Operation(summary = "001_", description = "인증번호 전송")
     @PostMapping("/message/send")
     public SuccessResponse<String> sendAuthNum(@RequestBody @Valid SendAuthNumReq sendAuthNumReq){
         messageService.sendAuthNum(sendAuthNumReq);
-        return SuccessResponse.successResponse("성공");
+        return SuccessResponse.successResponse("인증번호가 전송되었습니다.");
     }
 
     @Operation(summary = "001_", description = "인증번호 확인")
     @PostMapping("/message/verify")
     public SuccessResponse<String> verifyAuthNum(@RequestBody @Valid VerifyAuthNumReq verifyAuthNumReq){
         messageService.verifyAuthNum(verifyAuthNumReq);
-        return SuccessResponse.successResponse("성공");
+        return SuccessResponse.successResponse("인증 성공");
+    }
+
+    @Operation(summary = "001_", description = "아이디 중복체크")
+    @GetMapping("/check/account")
+    public SuccessResponse<String> checkAccount(@RequestParam String account){
+        authService.checkAccount(account);
+        return SuccessResponse.successResponse("사용 가능한 아이디입니다.");
     }
 }
