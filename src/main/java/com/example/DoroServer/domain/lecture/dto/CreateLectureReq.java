63dd9a140e003af8d53a56912b6828282af8178b
weightsforfun <giyoung914@gmail.com>
2023-04-09 13:42:36 +0900

    Fix: createLecture req 수정 및 중복 파일 제거 CreateLectureReq builder 수정 CreateLectureReq notblank로 수정 중복 dto 제거

--- src/main/java/com/example/DoroServer/domain/lecture/dto/CreateLectureReq.java
+++ src/main/java/com/example/DoroServer/domain/lecture/dto/CreateLectureReq.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,50 +18,50 @@ import lombok.ToString;
 @NoArgsConstructor
 public class CreateLectureReq {
 
-    @NotEmpty
+    @NotBlank
     private String title; // 강의 제목
-    @NotEmpty
+    @NotBlank
     private String institution; // 강의 기관
 
-    @NotEmpty
+    @NotBlank
     private String city; // 강의 도시
 
-    @NotEmpty
+    @NotBlank
     private String studentGrade; // 강의 대상
 
-    @NotEmpty
+    @NotBlank
     private String studentNumber; // 인원수
 
-    @NotEmpty
+    @NotBlank
     private String mainTutor; // 강의 메인 강사 수
 
-    @NotEmpty
+    @NotBlank
     private String subTutor; // 강의 서브 강사 수
-    @NotEmpty
+    @NotBlank
     private String staff; // 강의 스태프 수
-    @NotEmpty
+    @NotBlank
     private String payment; //강사 급여
-    @NotEmpty
+    @NotBlank
     private String time; // 시간
-    @NotEmpty
+    @NotBlank
     private List<LocalDate> lectureDates = new ArrayList<>(); // 강의 날짜
-    @NotEmpty
+    @NotBlank
     private LocalDateTime enrollStartDate; // 강의 등록 시작 날짜
-    @NotEmpty
+    @NotBlank
     private LocalDateTime enrollEndDate; // 강의 등록 종료 날짜
 
     private LectureContentDto lectureContentDto;
     public Lecture toEntity(){
         return Lecture.builder()
-                .institution("institution")
-                .city("city")
-                .studentGrade("studentGrade")
-                .studentNumber("studentNumber")
-                .mainTutor("mainTutor")
-                .subTutor("subTutor")
-                .staff("staff")
-                .payment("payment")
-                .time("time")
+                .institution(institution)
+                .city(city)
+                .studentGrade(studentGrade)
+                .studentNumber(studentNumber)
+                .mainTutor(mainTutor)
+                .subTutor(subTutor)
+                .staff(staff)
+                .payment(payment)
+                .time(time)
                 .lectureDate(
                         LectureDate.builder()
                                 .lectureDates(lectureDates)
