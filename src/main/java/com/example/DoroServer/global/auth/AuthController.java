32c16ab87c82e06ea11e0b480832a06362ea796a
Ju-yeop <dhwnduq12@naver.com>
2023-04-10 17:59:57 +0900

    Feat: AuthController - login 작업 중 - login - createAccessToken - createRefreshToken
    - authenticate 내부 인증 에러

--- src/main/java/com/example/DoroServer/global/auth/AuthController.java
+++ src/main/java/com/example/DoroServer/global/auth/AuthController.java
@@ -2,16 +2,28 @@ package com.example.DoroServer.global.auth;
 
 import com.example.DoroServer.domain.user.repository.UserRepository;
 import com.example.DoroServer.global.auth.dto.JoinReq;
+import com.example.DoroServer.global.auth.dto.LoginReq;
 import com.example.DoroServer.global.auth.dto.SendAuthNumReq;
 import com.example.DoroServer.global.auth.dto.VerifyAuthNumReq;
 import com.example.DoroServer.global.common.SuccessResponse;
+import com.example.DoroServer.global.jwt.CustomUserDetailsService;
+import com.example.DoroServer.global.jwt.JwtTokenProvider;
+import com.example.DoroServer.global.jwt.RedisService;
 import com.example.DoroServer.global.message.MessageService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
+import java.time.Duration;
 import javax.validation.Valid;
+import kotlinx.serialization.BinaryFormat;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,13 +38,14 @@ public class AuthController {
     private final UserRepository userRepository;
     private final MessageService messageService;
     private final AuthService authService;
+    private final JwtTokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final RedisService redisService;
 
-    /**
-     * ReponseEntity 사용 - header에 Token 추가
-     * Access 토큰은 헤더에 저장 Refresh 토큰은 Redis 저장
-     */
     @Operation(summary = "001_01", description = "회원가입")
-    public ResponseEntity<?> join (@RequestBody @Valid JoinReq joinReq){
+    @PostMapping("/join")
+    public SuccessResponse<String> join (@RequestBody @Valid JoinReq joinReq){
         /**
          * 인증 번호 저장
          * 전화번호 인증을 클릭하면 레디스에 인증번호을 저장하고
@@ -41,8 +54,29 @@ public class AuthController {
          * 회원가입을 할 때 RequestBody 값으로 들어온 phoneNumber가 인증된 전화번호 테이블에 존재하는
          * 전화번호인지 확인한다.
          */
+        authService.checkAccount(joinReq.getAccount());
+        authService.join(joinReq);
+        return SuccessResponse.successResponse("회원가입 완료");
+    }
 
-        return ResponseEntity.ok("회원가입 완료");
+    @Operation(summary = "001_02", description = "로그인")
+    @PostMapping("/login")
+    public ResponseEntity<?> login (@RequestBody @Valid LoginReq loginReq){
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+            loginReq.getAccount(), loginReq.getPassword());
+        log.info("AuthenticationToken.getName={}", authenticationToken.getName());
+        log.info("AuthenticationToken.getCredentials={}", authenticationToken.getCredentials());
+        log.info("AuthenticationToken={}", authenticationToken);
+        String accessToken = createAccessToken(authenticationToken);
+        String refreshToken = createRefreshToken(authenticationToken);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer " + accessToken);
+
+        redisService.setValues(loginReq.getAccount(), refreshToken, Duration.ofDays(180));
+
+        return ResponseEntity.ok()
+            .headers(httpHeaders).build();
     }
 
     @Operation(summary = "001_", description = "인증번호 전송")
@@ -65,4 +99,16 @@ public class AuthController {
         authService.checkAccount(account);
         return SuccessResponse.successResponse("사용 가능한 아이디입니다.");
     }
+    private String createAccessToken(UsernamePasswordAuthenticationToken authenticationToken) {
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        log.info("AccessToken 생성 준비 끝");
+        return tokenProvider.createAccessToken(authentication.getName(), authentication.getAuthorities());
+    }
+    private String createRefreshToken(UsernamePasswordAuthenticationToken authenticationToken) {
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(authenticationToken.getName());
+        return tokenProvider.createRefreshToken(userDetails.getUsername(), userDetails.getAuthorities());
+    }
+
+
 }
