07977ef87c92d044a8de1c71e7e4ff4f90b5a752
Ju-yeop <dhwnduq12@naver.com>
2023-04-10 17:56:08 +0900

    Feat: UserRepository - existsByAccount 추가

--- src/main/java/com/example/DoroServer/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/DoroServer/domain/user/repository/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByAccount(String account);
+    Boolean existsByAccount(String account);
 }
