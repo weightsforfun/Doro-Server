0864ab9e3c24dd3ca69d742b0e7e36e192b91f6c
Ju-yeop <dhwnduq12@naver.com>
2023-04-10 17:58:39 +0900

    Feat: Code 클래스에 AuthError 코드 추가

--- src/main/java/com/example/DoroServer/global/exception/Code.java
+++ src/main/java/com/example/DoroServer/global/exception/Code.java
@@ -20,6 +20,11 @@ public enum Code {
     JWT_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH004","토큰이 만료되었습니다."),
     JWT_UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH006","지원하지 않는 JWT 토큰입니다."),
     JWT_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "AUTH007","유효한 JWT 토큰이 없습니다."),
+    UNAUTHORIZED_PHONE_NUMBER(HttpStatus.UNAUTHORIZED, "AUTH008", "인증되지 않은 전화번호입니다."),
+    EXIST_ACCOUNT(HttpStatus.CONFLICT, "AUTH009", "이미 존재하는 아이디입니다."),
+    PASSWORD_DID_NOT_MATCH(HttpStatus.BAD_REQUEST, "AUTH010", "비밀번호가 일치하지 않습니다."),
+    DORO_ADMIN_AUTH_FAILED(HttpStatus.UNAUTHORIZED, "AUTH011", "관리자 인증번호가 일치하지 않습니다."),
+    DORO_USER_AUTH_FAILED(HttpStatus.UNAUTHORIZED, "AUTH012", "도로 인증번호가 일치하지 않습니다."),
     //USER 관련 오류 U001,U002...
 
     //강의 관련 오류 L001,L002..
