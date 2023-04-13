a90e2bccad43e8936a7231575a7684323a8f14ba
BangTtagGum <lionalmessi@naver.com>
2023-04-13 19:48:23 +0900

    Feat: Announcement Entity 수정
    메소드 추가

--- src/main/java/com/example/DoroServer/domain/announcement/entity/Announcement.java
+++ src/main/java/com/example/DoroServer/domain/announcement/entity/Announcement.java
@@ -1,5 +1,7 @@
 package com.example.DoroServer.domain.announcement.entity;
 
+import com.example.DoroServer.domain.announcement.dto.AnnouncementReq;
+import com.example.DoroServer.domain.announcement.dto.AnnouncementRes;
 import com.example.DoroServer.domain.base.BaseEntity;
 import lombok.*;
 
@@ -8,7 +10,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
@@ -26,8 +27,24 @@ public class Announcement extends BaseEntity {
     private String title; // 공지 제목
 
     @NotBlank
-    private String content; // 공지 내용
+    private String body; // 공지 내용
 
     private String  picture; // 공지 첨부 사진 - 업로드 사이즈 제한 추가 필요
 
+
+    public AnnouncementRes toRes() {
+        return AnnouncementRes.builder()
+                .id(id)
+                .title(title)
+                .body(body)
+                .picture(picture)
+                .createdAt(getCreatedAt())
+                .lastModifiedAt(getLastModifiedAt())
+                .build();
+    }
+    public void update(AnnouncementReq announcementReq) {
+        this.title = announcementReq.getTitle();
+        this.body = announcementReq.getBody();
+        this.picture = announcementReq.getPicture();
+    }
 }
