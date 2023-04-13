72f42bd4dc323581c98d18de11f63c54ac1bd88d
BangTtagGum <lionalmessi@naver.com>
2023-04-13 19:29:53 +0900

    Feat: AnnouncementRepository 추가
    AnnouncementRes 조회 메소드 추가

---
+++ src/main/java/com/example/DoroServer/domain/announcement/repository/AnnouncementRepository.java
@@ -0,0 +1,15 @@
+package com.example.DoroServer.domain.announcement.repository;
+
+import com.example.DoroServer.domain.announcement.dto.AnnouncementRes;
+import com.example.DoroServer.domain.announcement.entity.Announcement;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
+
+    @Query("select new com.example.DoroServer.domain.announcement.dto.AnnouncementRes(a.id, a.title, a.body, a.picture,a.createdAt,a.lastModifiedAt) from Announcement a")
+    List<AnnouncementRes> findAllRes();
+
+}
