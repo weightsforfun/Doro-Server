10ce7e781c0ccf4cf768d2b7defd7b4795bdd574
Ju-yeop <dhwnduq12@naver.com>
2023-04-16 23:15:46 +0900

    Feat: @Secured 사용 설정

--- src/main/java/com/example/DoroServer/global/config/SecurityConfig.java
+++ src/main/java/com/example/DoroServer/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.DoroServer.global.jwt.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -16,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig{
 
     private final JwtTokenProvider jwtTokenProvider;
