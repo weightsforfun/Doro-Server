2b2548d389c14f11f9a6a2934a8535ea2df18576
weightsforfun <giyoung914@gmail.com>
2023-04-23 12:32:22 +0900

    Fix: DateTimeFromat 추가 DateTimeFromat 추가

--- src/main/java/com/example/DoroServer/domain/lecture/dto/FindAllLecturesCond.java
+++ src/main/java/com/example/DoroServer/domain/lecture/dto/FindAllLecturesCond.java
@@ -1,12 +1,18 @@
 package com.example.DoroServer.domain.lecture.dto;
 
 import java.time.LocalDate;
+import java.util.List;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
+@Setter
 public class FindAllLecturesCond {
-    private String city;
+    private List<String> city;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
 
 }
