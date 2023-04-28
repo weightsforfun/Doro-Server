c81d92fde4b003d3baf19cad72cf746e680e3986
weightsforfun <giyoung914@gmail.com>
2023-04-28 20:34:04 +0900

    Fix: Fetch results-> fetch 로 변경 Fetch results-> fetch 로 변경

--- src/main/java/com/example/DoroServer/domain/lecture/repository/LectureRepositoryImpl.java
+++ src/main/java/com/example/DoroServer/domain/lecture/repository/LectureRepositoryImpl.java
@@ -4,29 +4,24 @@ import static com.example.DoroServer.domain.lecture.entity.QLecture.*;
 
 import com.example.DoroServer.domain.lecture.dto.FindAllLecturesCond;
 import com.example.DoroServer.domain.lecture.entity.Lecture;
-import com.example.DoroServer.domain.lecture.entity.QLectureDate;
 import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.DatePath;
-import com.querydsl.core.types.dsl.ListPath;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
+
 
 @Repository
 @RequiredArgsConstructor
 public class LectureRepositoryImpl implements LectureRepositoryCustom{
     private final JPAQueryFactory queryFactory;
     public Page<Lecture> findAllLecturesWithFilter(FindAllLecturesCond condition, Pageable pageable){
-        QueryResults<Lecture> results= queryFactory
+        List<Lecture> results= queryFactory
                 .select(lecture)
                 .from(lecture)
                 .where(
@@ -35,10 +30,9 @@ public class LectureRepositoryImpl implements LectureRepositoryCustom{
                 )
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
-                .fetchResults();
-        List<Lecture> content = results.getResults();
-        long total = results.getTotal();
-        return new PageImpl<>(content,pageable,total);
+                .fetch();
+
+        return new PageImpl<>(results,pageable,results.size());
 
     }
    private BooleanBuilder containCity(List<String> cities){
