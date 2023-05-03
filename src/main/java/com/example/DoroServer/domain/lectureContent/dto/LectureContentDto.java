689d10f2260dae6a62e0e81208c98ce8e043eb83
weightsforfun <giyoung914@gmail.com>
2023-05-03 21:45:23 +0900

    Fix: LectureContent dto 에 id 추가 LectureContent dto 에 id 추가

--- src/main/java/com/example/DoroServer/domain/lectureContent/dto/LectureContentDto.java
+++ src/main/java/com/example/DoroServer/domain/lectureContent/dto/LectureContentDto.java
@@ -16,30 +16,18 @@ import lombok.ToString;
 @AllArgsConstructor
 public class LectureContentDto {
 
-    @NotEmpty
+
     private String kit; // 강의 사용 키트
-    @NotEmpty
+
     private String detail; // 강의 세부 구성
-    @NotEmpty
+
     private String remark; // 강의 기타 사항
-    @NotEmpty
+
     private String requirement; // 강의 자격 요건
 
-    public  LectureContent toEntity() {
-        return LectureContent.builder()
-                .kit(kit)
-                .detail(detail)
-                .remark(remark)
-                .requirement(requirement)
-                .build();
-    }
+    private Long id;
 
-    public static LectureContentDto fromEntity(LectureContent lectureContent) {
-        return LectureContentDto.builder()
-                .kit(lectureContent.getKit())
-                .detail(lectureContent.getDetail())
-                .remark(lectureContent.getRemark())
-                .requirement(lectureContent.getRequirement())
-                .build();
-    }
+
+
+
 }
