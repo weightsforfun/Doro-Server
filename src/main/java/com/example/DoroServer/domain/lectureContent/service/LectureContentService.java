88eb18542003fc494759515a83c738d6a0abad85
weightsforfun <giyoung914@gmail.com>
2023-05-03 21:59:59 +0900

    Feat: updateLectureContent service 로직 추가 updateLectureContent service 로직 추가

--- src/main/java/com/example/DoroServer/domain/lectureContent/service/LectureContentService.java
+++ src/main/java/com/example/DoroServer/domain/lectureContent/service/LectureContentService.java
@@ -3,11 +3,15 @@ package com.example.DoroServer.domain.lectureContent.service;
 import com.example.DoroServer.domain.lecture.repository.LectureRepository;
 import com.example.DoroServer.domain.lectureContent.dto.LectureContentDto;
 import com.example.DoroServer.domain.lectureContent.dto.LectureContentMapper;
+import com.example.DoroServer.domain.lectureContent.dto.UpdateLectureContentReq;
 import com.example.DoroServer.domain.lectureContent.entity.LectureContent;
 import com.example.DoroServer.domain.lectureContent.repository.LectureContentRepository;
+import com.example.DoroServer.global.exception.BaseException;
+import com.example.DoroServer.global.exception.Code;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class LectureContentService {
     private final LectureContentRepository lectureContentRepository;
     private final LectureContentMapper lectureContentMapper;
+    private final ModelMapper modelMapper;
     public Long createLecture(LectureContentDto lectureContentDto){
         LectureContent lectureContent = lectureContentMapper.toLectureContent(lectureContentDto);
         LectureContent savedLectureContent = lectureContentRepository.save(lectureContent);
@@ -28,5 +33,12 @@ public class LectureContentService {
                 .map(lectureContent -> lectureContentMapper.toLectureContentDto(lectureContent))
                 .collect(Collectors.toList());
         return lectureContentResList;
+    }
+
+    public Long updateLectureContent(Long id, UpdateLectureContentReq updateLectureContentReq){
+        LectureContent lectureContent = lectureContentRepository.findById(id)
+                .orElseThrow(() -> new BaseException(Code.LECTURE_CONTENT_NOT_FOUND));
+        modelMapper.map(updateLectureContentReq,lectureContent);
+        return lectureContent.getId();
     }
 }
