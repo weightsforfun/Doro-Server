d0d7fd110ef09868b5104f4833609f816795957d
weightsforfun <giyoung914@gmail.com>
2023-05-03 21:44:23 +0900

    Fix: LectureContent service mapper 적용 LectureContent service mapper 적용

--- src/main/java/com/example/DoroServer/domain/lectureContent/service/LectureContentService.java
+++ src/main/java/com/example/DoroServer/domain/lectureContent/service/LectureContentService.java
@@ -2,6 +2,7 @@ package com.example.DoroServer.domain.lectureContent.service;
 
 import com.example.DoroServer.domain.lecture.repository.LectureRepository;
 import com.example.DoroServer.domain.lectureContent.dto.LectureContentDto;
+import com.example.DoroServer.domain.lectureContent.dto.LectureContentMapper;
 import com.example.DoroServer.domain.lectureContent.entity.LectureContent;
 import com.example.DoroServer.domain.lectureContent.repository.LectureContentRepository;
 import java.util.List;
@@ -15,15 +16,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class LectureContentService {
     private final LectureContentRepository lectureContentRepository;
+    private final LectureContentMapper lectureContentMapper;
     public Long createLecture(LectureContentDto lectureContentDto){
-        LectureContent lectureContent = lectureContentDto.toEntity();
+        LectureContent lectureContent = lectureContentMapper.toLectureContent(lectureContentDto);
         LectureContent savedLectureContent = lectureContentRepository.save(lectureContent);
         return savedLectureContent.getId();
     }
     public List<LectureContentDto> findAllLectureContents(){
         List<LectureContent> lectureContentList = lectureContentRepository.findAll();
         List<LectureContentDto> lectureContentResList = lectureContentList.stream()
-                .map(LectureContentDto::fromEntity)
+                .map(lectureContent -> lectureContentMapper.toLectureContentDto(lectureContent))
                 .collect(Collectors.toList());
         return lectureContentResList;
     }
