4cbda615d2e2f59bc68c0b34453fe405ba3a4111
BangTtagGum <lionalmessi@naver.com>
2023-05-05 22:07:36 +0900

    Feat: NotificationRepository 메소드 추가
    공용 알림만 조회하는 findAllPublicRes 추가

--- src/main/java/com/example/DoroServer/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/DoroServer/domain/notification/repository/NotificationRepository.java
@@ -5,10 +5,11 @@ import com.example.DoroServer.domain.notification.entity.Notification;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     // NotificationRes 객체 모두를 직접 조회하는 메서드
-    @Query("select new com.example.DoroServer.domain.notification.dto.NotificationRes(n.id,n.title,n.body,n.isRead,n.createdAt,n.lastModifiedAt) from Notification n")
-    List<NotificationRes> findAllRes();
+    @Query("select new com.example.DoroServer.domain.notification.dto.NotificationRes(n.id,n.title,n.body,n.isRead,n.createdAt,n.lastModifiedAt) from Notification n where n.isPublic = :isPublic")
+    List<NotificationRes> findAllPublicRes(@Param("isPublic") Boolean isPublic);
 }
