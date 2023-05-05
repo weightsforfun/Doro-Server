e94c9a1775eacbf311b319e1405520c75463e21f
BangTtagGum <lionalmessi@naver.com>
2023-05-05 23:03:42 +0900

    Feat: UserNotificationRepository 추가
    UserNotificationRepository 추가

--- src/main/java/com/example/DoroServer/domain/userNotification/repository/UserNotificationRepository.java
+++ src/main/java/com/example/DoroServer/domain/userNotification/repository/UserNotificationRepository.java
@@ -1,2 +1,20 @@
-package com.example.DoroServer.domain.userNotification.repository;public interface UserNotificationRepository {
+package com.example.DoroServer.domain.userNotification.repository;
+
+import com.example.DoroServer.domain.lecture.entity.Lecture;
+import com.example.DoroServer.domain.user.entity.User;
+import com.example.DoroServer.domain.userNotification.entity.UserNotification;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserNotificationRepository extends JpaRepository<UserNotification, Long> {
+
+    @Query(value="select un from UserNotification un"
+            + " join fetch un.user u"
+            + " join fetch un.notification n"
+            + " where u.id = :id"
+    )
+    List<UserNotification> findUserNotificationsByUserId(@Param("id") Long userId);
 }
