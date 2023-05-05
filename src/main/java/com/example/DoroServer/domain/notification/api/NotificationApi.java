e8546167cb4d4384bd8e3c42b6245220722d14be
BangTtagGum <lionalmessi@naver.com>
2023-05-06 04:34:30 +0900

    Feat: Swagger 메소드 설명 추가
    Notification 설명 추가

--- src/main/java/com/example/DoroServer/domain/notification/api/NotificationApi.java
+++ src/main/java/com/example/DoroServer/domain/notification/api/NotificationApi.java
@@ -3,19 +3,24 @@ package com.example.DoroServer.domain.notification.api;
 import com.example.DoroServer.domain.notification.dto.NotificationContentReq;
 import com.example.DoroServer.domain.notification.dto.NotificationRes;
 import com.example.DoroServer.domain.notification.service.NotificationService;
-import com.example.DoroServer.domain.user.dto.FindUserRes;
 import com.example.DoroServer.domain.user.entity.User;
 import com.example.DoroServer.domain.user.repository.UserRepository;
-import com.example.DoroServer.domain.user.service.UserService;
 import com.example.DoroServer.domain.userNotification.service.UserNotificationService;
 import com.example.DoroServer.global.common.SuccessResponse;
 import com.example.DoroServer.global.exception.BaseException;
 import com.example.DoroServer.global.exception.Code;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +29,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "알림📢")
 @Slf4j
 @RestController
 @RequestMapping("/notifications")
@@ -39,20 +45,28 @@ public class NotificationApi {
     private final UserRepository userRepository;
 
     // 모든 Notification 조회 메소드
+    @ApiOperation(value = "유저의 알림 전체 조회", notes = "userId를 전달해서 해당 유저의 알림 전체를 조회합니다.")
     @GetMapping("/{userId}")
-    public SuccessResponse findUserNotifications(@PathVariable("userId") Long userId) {
+    public SuccessResponse findUserNotifications(@PathVariable("userId") Long userId,
+            @PageableDefault(page = 0,size = 10,sort = "createdAt", direction = Direction.DESC) Pageable pageable) {
         // 공용 알림만 전체 조회
-        List<NotificationRes> notifications = notificationService.findPublicNotifications();
+        List<NotificationRes> publicNotifications = notificationService.findPublicNotifications();
 
         // 유저별 알림 조희
         List<NotificationRes> userNotifications = notificationService.findUserNotifications(userId);
 
-        // 병합 후 조회된 전체 알림 반환
-        notifications. addAll(userNotifications);
-        return SuccessResponse.successResponse(notifications);
+        // 병합 후 조회된 전체 알림 페이징 후반환
+        publicNotifications.addAll(userNotifications);
+        Collections.sort(publicNotifications, Comparator.comparing(NotificationRes::getCreatedAt, Comparator.reverseOrder()));
+            List<NotificationRes> notifications = publicNotifications.stream()
+                    .skip((long) pageable.getPageNumber() * pageable.getPageSize()).limit(pageable.getPageSize())
+                    .collect(Collectors.toList());
+            return SuccessResponse.successResponse(notifications);
     }
 
     // FCM 서버에 알림 전송요청
+    @ApiOperation(value = "알림 전송", notes = "알림 제목(title), 내용(body), 전송할 유저들의 아이디(userIds)를 입력받아 알림을 생성합니다."
+            + " userIds를 적지 않거나, 비워두면 사용자 전체에게 전송됩니다.")
     @PostMapping
     public SuccessResponse pushNotifications(
             @RequestBody @Valid NotificationContentReq notificationContentReq
