5322a175a912c21f9c36365aece558e7ec7e1076
weightsforfun <giyoung914@gmail.com>
2023-06-30 11:51:09 +0900

    Fix : lecture content valid 어노테이션 수정 및 적용
    lecture content valid 어노테이션 수정 및 적용

--- src/main/java/com/example/DoroServer/domain/lectureContent/api/LectureContentApi.java
+++ src/main/java/com/example/DoroServer/domain/lectureContent/api/LectureContentApi.java
@@ -7,6 +7,7 @@ import com.example.DoroServer.domain.lectureContent.service.LectureContentServic
 import com.example.DoroServer.global.common.SuccessResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,13 +16,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/lecture-contents")
 @RequiredArgsConstructor
+@Validated
 public class LectureContentApi {
     private final LectureContentService lectureContentService;
     @PostMapping()
-    public SuccessResponse createLectureContent(@RequestBody LectureContentDto lectureContentDto){
+    public SuccessResponse createLectureContent(@RequestBody @Valid LectureContentDto lectureContentDto){
         Long lectureId = lectureContentService.createLecture(lectureContentDto);
         return SuccessResponse.successResponse(lectureId);
     }
