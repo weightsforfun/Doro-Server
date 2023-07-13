ff6f8ba278f081d907f8f086fcdc4d8f5413c5a5
weightsforfun <giyoung914@gmail.com>
2023-07-13 15:31:13 +0900

    Feat: userLecture DTO builder 추가 테스트 코드를 위한 userLecture DTO builder 추가

--- src/main/java/com/example/DoroServer/domain/userLecture/dto/CreateTutorReq.java
+++ src/main/java/com/example/DoroServer/domain/userLecture/dto/CreateTutorReq.java
@@ -5,10 +5,13 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@Builder
 @EqualsAndHashCode
 public class CreateTutorReq {
     @NotNull
