b8232c795ef40fce6d9957152b3e6bf3acdb0129
weightsforfun <giyoung914@gmail.com>
2023-08-16 13:37:11 +0900

    Feat: 구독, 공지용 알림 전송 로직 생성 #94 전체 전송 로직 구독 전송 방법으로 변경 그에 따른 구독 관리 로직 추가

--- src/main/java/com/example/DoroServer/domain/notification/service/NotificationServiceRefact.java
+++ src/main/java/com/example/DoroServer/domain/notification/service/NotificationServiceRefact.java
@@ -1,41 +1,86 @@
 package com.example.DoroServer.domain.notification.service;
 
 
+import com.example.DoroServer.domain.notification.dto.NotificationReq;
+import com.example.DoroServer.domain.notification.entity.NotificationType;
+import com.example.DoroServer.domain.token.service.TokenService;
 import com.example.DoroServer.global.exception.BaseException;
 import com.example.DoroServer.global.exception.Code;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.*;
 
-import com.google.firebase.messaging.Notification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
 public class NotificationServiceRefact {
-    public String sendNotificationToOne(){
+    private final FirebaseMessaging firebaseMessaging;
+    private final TokenService tokenService;
+
+
+    public String sendNotificationToOne(String token, NotificationReq notificationReq){
         Notification noti = Notification.builder()
                 .setBody("hey")
                 .setTitle("youn's notification")
                 .build();
         Message mes = Message.builder()
-                .setToken("123")
+                .setToken(token)
                 .setNotification(noti)
                 .build();
 
         try {
-            String response = FirebaseMessaging.getInstance().send(mes);
+            String response = firebaseMessaging.send(mes);
             return response;
         } catch (FirebaseMessagingException e) {
             log.info(String.valueOf(e));
             throw new BaseException(Code.BAD_REQUEST);
         }
 
+    }
+
+   public void sendAnnouncementNotification(NotificationReq notificationReq){
+       List<String> allTokens = tokenService.findAllTokens()
+               .stream()
+               .map(tokenDto -> tokenDto.getToken())
+               .collect(Collectors.toList());
+
+       Notification noti = Notification.builder()
+               .setBody("hey")
+               .setTitle("youn's notification")
+               .build();
+
+       Message mes = Message.builder()
+               .setNotification(noti)
+               .setTopic(NotificationType.ANNOUNCEMENT.toString())
+               .build();
+       try {
+           firebaseMessaging.send(mes);
+       }catch (FirebaseMessagingException e){
+           throw new BaseException(Code.NOTIFICATION_PUSH_FAIL);
+       }
+
+   }
+
+   public void subscribe(NotificationType notificationType,String token){
+       try {
+           firebaseMessaging.subscribeToTopic(List.of("asd"),notificationType.toString());
+       } catch (FirebaseMessagingException e) {
+           throw new BaseException(Code.FORBIDDEN);
+       }
+   }
+
+    public void unsubscribe(NotificationType notificationType,String token){
+        try {
+            firebaseMessaging.unsubscribeFromTopic(List.of("asd"),notificationType.toString());
+        } catch (FirebaseMessagingException e) {
+            throw new BaseException(Code.FORBIDDEN);
+        }
     }
 }
