6fd9e2084e8a0fe9ad1e33b40050fb387714e979
weightsforfun <giyoung914@gmail.com>
2023-08-18 21:39:50 +0900

    Feat: NotificationApi 테스트 API 및 알림 전체 전송 API 추가 #94 NotificationApi 테스트 API 및 알림 전체 전송 API 추가

--- src/main/java/com/example/DoroServer/domain/notification/api/NotificationApi.java
+++ src/main/java/com/example/DoroServer/domain/notification/api/NotificationApi.java
@@ -1,26 +1,18 @@
 package com.example.DoroServer.domain.notification.api;
 
 import com.example.DoroServer.domain.notification.dto.NotificationContentReq;
-import com.example.DoroServer.domain.notification.dto.NotificationReq;
-import com.example.DoroServer.domain.notification.dto.NotificationRes;
 import com.example.DoroServer.domain.notification.entity.SubscriptionType;
-import com.example.DoroServer.domain.notification.service.NotificationService;
 import com.example.DoroServer.domain.notification.service.NotificationServiceRefact;
 import com.example.DoroServer.domain.user.repository.UserRepository;
-import com.example.DoroServer.domain.userNotification.service.UserNotificationService;
 import com.example.DoroServer.global.common.SuccessResponse;
+import com.google.firebase.messaging.TopicManagementResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,57 +25,91 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class NotificationApi {
 
-    private final NotificationService notificationService;
+
     private final NotificationServiceRefact notificationServiceRefact;
 
-    private final UserNotificationService userNotificationService;
 
-    // UserService에 id 로 user 단일 조회 있으면 service로 받아올 수 있는데 없어서 repository 받아옴
-    // private final UserService userService;
+
+
     private final UserRepository userRepository;
 
-    // 모든 Notification 조회 메소드
-    @ApiOperation(value = "유저의 전체 알림 조회", notes = "userId를 전달해서 해당 유저의 알림 전체를 조회합니다. 파라미터로 page랑 size 전달하시면 페이징 됩니다. 이게 Swagger가 잘 안돼서 Postman으로 테스트 해보시는게 나을거에요 Swagger는 이상하게 page랑 size를 인식못하네요")
-    @GetMapping("/{userId}")
-    public SuccessResponse findUserNotifications(@PathVariable("userId") Long userId,
-            @PageableDefault(page = 0,size = 10,sort = "id", direction = Direction.DESC) Pageable pageable) {
-
-        // 유저별 알림 조희
-        List<NotificationRes> userNotifications = notificationService.findUserNotifications(userId, pageable);
-
-        // 병합 후 조회된 전체 알림 페이징 후반환
-        return SuccessResponse.successResponse(userNotifications);
-    }
-
-    // FCM 서버에 알림 전송요청
-    @ApiOperation(value = "알림 전송", notes = "알림 제목(title), 내용(body), 전송할 유저들의 아이디(userIds)를 입력받아 알림을 생성합니다."
-            + " userIds를 적지 않거나, 비워두면 사용자 전체에게 전송됩니다.")
+    @ApiOperation(value = "알림 전체 전송", notes =
+            "알림 제목(title), 내용(body)를 입력받아 알림을 생성합니다.")
     @Secured("ROLE_ADMIN")
     @PostMapping
-    public SuccessResponse pushNotifications(
-            @RequestBody @Valid NotificationContentReq notificationContentReq
-            ) {
-        if (notificationContentReq.getUserIds() == null || notificationContentReq.getUserIds().isEmpty()) {
-            // 유저 ID 리스트가 비어 있으면 모든 사용자에게 알림 전송
-            notificationService.sendNotificationToAll(notificationContentReq, SubscriptionType.NOTIFICATION,null);
-        } else {
-            // 유저 ID 리스트에 있는 사용자에게만 알림 전송
-            notificationService.sendNotificationsToSelectedUsers(notificationContentReq, SubscriptionType.NOTIFICATION);
-        }
-        return SuccessResponse.successResponse("Notification push complete");
+    public SuccessResponse sendNotificationToAll(
+            @RequestBody @Valid NotificationContentReq notificationContentReq) {
+
+        String response = notificationServiceRefact.sendNotificationToAllUsers(
+                notificationContentReq,1L);
+
+        return SuccessResponse.successResponse(response);
     }
-    @ApiOperation(value = "유저의 알림 읽음 처리", notes = "notificationId를 전달해서 해당 알림을 읽음처리 합니다.")
-    @PostMapping("/{notificationId}/doRead")
-    public SuccessResponse findUserNotifications(@PathVariable("notificationId") Long notificationId) {
-        notificationService.readNotification(notificationId);
-        return SuccessResponse.successResponse("read Notification complete");
+
+
+
+    @GetMapping("/sendAll")
+    public SuccessResponse sendAll() {
+
+        NotificationContentReq notificationContentReq = NotificationContentReq.builder()
+                .body("hi")
+                .title("Announcement")
+                .build();
+
+        String response = notificationServiceRefact.sendNotificationToAllUsers(
+                notificationContentReq,1L);
+
+        return SuccessResponse.successResponse(response);
     }
 
     @GetMapping("/test")
-    public SuccessResponse testAPI(){
-        NotificationReq notificationReq = NotificationReq.builder().body("a").build();
-        String response = notificationServiceRefact.sendNotificationToOne("avc",notificationReq);
+    public SuccessResponse testAPI() {
+        NotificationContentReq notificationContentReq = NotificationContentReq.builder()
+                .body("hi")
+                .title("my name is")
+                .subscriptionType(SubscriptionType.ANNOUNCEMENT)
+                .build();
+        Long userId = notificationServiceRefact.sendNotificationToOne(
+                1L,1L,notificationContentReq);
+        return SuccessResponse.successResponse(userId);
+    }
+
+    @GetMapping("/subscribe")
+    public SuccessResponse subscribeTestAPI() {
+        TopicManagementResponse response = notificationServiceRefact.subscribe(
+                SubscriptionType.ANNOUNCEMENT,
+                "drnbi7uAR4Wh3jFK-W-T8g:APA91bEqBFnq8OU1eOB-2zh2AIWl77Bb5PDuyVuI5YhovSFYUcrakzWa5DvQHF9wlX2M7vPQRo7HHsGaDD0YTgiL7t1tA6XM7LdGqOdeJNYKcdhD4E7JQsV1-Bim2EhQzpi518XQmPpS");
         return SuccessResponse.successResponse(response);
     }
+
+
+    // 모든 Notification 조회 메소드
+//    @ApiOperation(value = "유저의 전체 알림 조회", notes = "userId를 전달해서 해당 유저의 알림 전체를 조회합니다. 파라미터로 page랑 size 전달하시면 페이징 됩니다. 이게 Swagger가 잘 안돼서 Postman으로 테스트 해보시는게 나을거에요 Swagger는 이상하게 page랑 size를 인식못하네요")
+//    @GetMapping("/{userId}")
+//    public SuccessResponse findUserNotifications(@PathVariable("userId") Long userId,
+//            @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.DESC) Pageable pageable) {
+//
+//        // 유저별 알림 조희
+//        List<NotificationRes> userNotifications = notificationService.findUserNotifications(userId,
+//                pageable);
+//
+//        // 병합 후 조회된 전체 알림 페이징 후반환
+//        return SuccessResponse.successResponse(userNotifications);
+//    }
+
+
+
+
+//    @ApiOperation(value = "유저의 알림 읽음 처리", notes = "notificationId를 전달해서 해당 알림을 읽음처리 합니다.")
+//    @PostMapping("/{notificationId}/doRead")
+//    public SuccessResponse findUserNotifications(
+//            @PathVariable("notificationId") Long notificationId) {
+//
+//        notificationService.readNotification(notificationId);
+//
+//        return SuccessResponse.successResponse("read Notification complete");
+//    }
+
+
 
 }
