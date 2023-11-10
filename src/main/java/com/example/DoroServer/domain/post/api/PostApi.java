e2cf71880ee3cdecc720b10650107392e08f9ee1
weightsforfun <giyoung914@gmail.com>
2023-11-10 21:32:00 +0900

    Feat: post API, Service 생성 #120 post API, Service 생성

--- src/main/java/com/example/DoroServer/domain/post/api/PostApi.java
+++ src/main/java/com/example/DoroServer/domain/post/api/PostApi.java
@@ -1,11 +1,21 @@
 package com.example.DoroServer.domain.post.api;
 
 
+import com.example.DoroServer.domain.post.dto.CreatePostReq;
+import com.example.DoroServer.domain.post.dto.FindAllPostRes;
+import com.example.DoroServer.domain.post.dto.FindPostRes;
+import com.example.DoroServer.domain.post.dto.UpdatePostReq;
 import com.example.DoroServer.domain.post.service.PostService;
+import com.example.DoroServer.global.common.SuccessResponse;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
 
 @Api(tags = "문의글📋")
 @RestController
@@ -16,14 +26,88 @@ public class PostApi {
     private final PostService postService;
 
     //문의글 생성
+    @PostMapping()
+    @ApiOperation(value = "문의글 생성",notes = "문의글 생성")
+    public SuccessResponse createPost(
+            @RequestBody @Valid CreatePostReq createPostReq
+    ) {
+        Long postId = postService.createPost(createPostReq);
+        return SuccessResponse.successResponse(postId);
+    }
+
     //문의글 수정
-    //문의글에 답변 작성(update answer)
+    @PatchMapping("/{id}")
+    @ApiOperation(value = "문의글 수정",notes = "바꾸고 싶은 필드만 넣어서 update해도 되고 값 다 넣어서 해도 됩니다. 게시물의 주인인지 확인하기 위해 currentpassowrd 에 유저가 입력한 비밀번호를 넣어줘야 합니다.")
+    public SuccessResponse updatePost(
+            @PathVariable("id") Long postId,
+            @RequestBody @Valid UpdatePostReq updatePostReq
+    ) {
+        Long updatedPostId = postService.updatePost(postId, updatePostReq);
+
+        return SuccessResponse.successResponse(updatedPostId);
+    }
+
+    //답변 작성
+    @PatchMapping("/{id}/answer")
+    @ApiOperation(value = "문의글 답변 작성",notes = "body 에 json 으로 key:answer , value:xxx 이런식으로 넣어주면됩니다")
+    public SuccessResponse updateAnswer(
+            @PathVariable("id") Long postId,
+            @RequestBody Map<String,String> answerMap) {
+        Long updatedPostId = postService.updateAnswer(postId, answerMap.get("answer"));
+
+        return SuccessResponse.successResponse(updatedPostId);
+    }
+
     //문의글 전부 조회(get all paging 사용)
+    @GetMapping()
+    @ApiOperation(value = "문의글 전체 조회",notes = "문의글 전체 조회 page 기본 사이즈 10,기본 page 0입니다. page,size 파라미터로 조절 가능")
+    public SuccessResponse findAllPosts(
+            @RequestParam(value="page", defaultValue = "0") int page,
+            @RequestParam(value="size",defaultValue = "10") int size
+            ) {
+
+        PageRequest pageRequest = PageRequest.of(page,size);
+
+        List<FindAllPostRes> allPost = postService.findAllPost(pageRequest);
+
+        return SuccessResponse.successResponse(allPost);
+    }
+
     //문의글 조회
+    @GetMapping("/{id}")
+    @ApiOperation(value = "문의글 조회",notes = "문의글 조회")
+    public SuccessResponse findPost(
+            @PathVariable("id") Long postId
+    ) {
+
+        FindPostRes findPostRes = postService.findPost(postId);
+
+        return SuccessResponse.successResponse(findPostRes);
+    }
+
     //문의글 삭제
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "문의글 삭제",notes = "문의글 삭제")
+    public SuccessResponse deletePost(
+            @PathVariable("id") Long postId
+    ) {
+        Long deletedPostId = postService.deletePost(postId);
+
+        return SuccessResponse.successResponse(deletedPostId);
+    }
 
     //문의글 비밀번호 조회 (매니저는 어떤 비번 치든 접근 허용)
+    @GetMapping("/{id}/password")
+    @ApiOperation(value = "비밀번호 체크",notes = "해당 문의글에 해당하는 비밀번호를 체크합니다. 일치하면 true, 틀리면 false를 반환합니다")
+    public SuccessResponse checkPassword(
+            @PathVariable("id") Long postId,
+            @RequestBody Map<String,String> passwordMap
+    ) {
 
+        boolean result = postService.checkPassword(postId, passwordMap.get("password"));
+
+        return SuccessResponse.successResponse(result);
+    }
 
 
 }
--- src/main/java/com/example/DoroServer/domain/post/service/PostService.java
+++ src/main/java/com/example/DoroServer/domain/post/service/PostService.java
@@ -1,15 +1,101 @@
 package com.example.DoroServer.domain.post.service;
 
 
+import com.example.DoroServer.domain.post.dto.*;
+import com.example.DoroServer.domain.post.entity.Post;
+import com.example.DoroServer.domain.post.repository.PostRepository;
+import com.example.DoroServer.global.exception.BaseException;
+import com.example.DoroServer.global.exception.Code;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 @Slf4j
 public class PostService {
+    private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
+    private final PostMapper postmapper;
+
+
+    public Long createPost(CreatePostReq createPostReq) {
+        //mapstruct로 entity 생성
+
+        Post post = postmapper.toPost(createPostReq);
+
+        Post savedPost = postRepository.save(post);
+
+        return savedPost.getId();
+    }
+
+
+    //답변, 게시글 수정에 모두 사용됩니다.(model mapper 에서 null 값은 업데이트를 안치게 적용)
+    public Long updatePost(Long postId, UpdatePostReq updatePostReq) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new BaseException(Code.BAD_REQUEST));
+
+        //작성자의 비밀번호가 일치하지 않습니다.
+        if(!post.getPassword().equals(updatePostReq.getCurrentPassword())){
+            throw new BaseException(Code.BAD_REQUEST);
+        }
+        //model mapper 적용
+        modelMapper.map(updatePostReq,post);
+
+        return post.getId();
+    }
+
+    public Long updateAnswer(Long postId,String answer){
+
+        Post post = postRepository.findById(postId).orElseThrow(() -> new BaseException(Code.BAD_REQUEST));
+
+        post.enrollAnswer(answer);
+
+        return post.getId();
+
+    }
+
+    public List<FindAllPostRes> findAllPost(Pageable pageable) {
+
+        Page<Post> posts = postRepository.findAll(pageable);
+        //post -> postRes 로 전환
+        List<FindAllPostRes> findAllPostResList = posts.stream().map(post -> postmapper.toFindAllPostRes(post)).collect(Collectors.toList());
+
+        // return postRes
+        return findAllPostResList;
+    }
+
+    public FindPostRes findPost(Long id) {
+
+        Post post = postRepository.findById(id).orElseThrow(() -> new BaseException(Code.BAD_REQUEST));
+
+        //post postRes로 전환
+        FindPostRes findPostRes = postmapper.toFindPostRes(post);
+        //return post
+        return findPostRes;
+    }
+
+    public Long deletePost(Long id){
+        postRepository.deleteById(id);
+        return id;
+    }
+
+    public boolean checkPassword(Long id, String password){
+
+        Post post = postRepository.findById(id).orElseThrow(() -> new BaseException(Code.USER_NOT_FOUND));
+
+        if(!post.getPassword().equals(password)){
+            return false;
+        }
+
+        return true;
+    }
 
 }
